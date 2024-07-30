499aa4ad4ab3ec11158b07fe43f0efedcf68367a
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2024-07-30 11:45:30 +0100

    working on the header responsiveness

---
+++ safe-ride/src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import BlackBtn from "../Button/BlackBtn/BlackBtn"
+import "./Header.css"
+import Logo from "../../assets/Saferide.svg";
+import Hamburger from "hamburger-react"
+import { useState } from "react";
+const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
+  return (
+    <header className="header">
+        <section className="header-container">
+            <nav className="navbar">
+
+                <div className="logo">
+                    <img src={Logo} alt="logo" />
+                </div>
+                <ul className="nav-links">
+                    <li><a href="/">Ride</a></li>
+                    <li><a href="/about">About us</a></li>
+                    <li><a href="/contact">Contact us</a></li>
+                </ul>
+
+
+            </nav>
+                <div className="header-btn-container">
+                    <button className="header-login" >Login</button>
+                    <BlackBtn>Sign up</BlackBtn>
+                </div>
+
+        </section>
+
+      
+    </header>
+  )
+}
+
+export default Header
