8027a18ab4073ef0d29c30e0a7af69f2072c5fe2
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2024-08-04 00:13:56 +0100

    working on the explore responsiveness

--- safe-ride/src/components/RequestBtn/Request.jsx
+++ safe-ride/src/components/RequestBtn/Request.jsx
@@ -3,10 +3,9 @@ import BlackBtn from '../Button/BlackBtn/BlackBtn'
 import "./Request.css"
 import GreenBtn from '../Button/GreenBtn/GreenBtn'
 
-const Request = ({children}) => {
+const Request = () => {
   return (
     <main className='main-request-container Request-custom'>
-        {children}
         <h1>Make Your Request</h1>
         <article className="request-btn-container">
             <BlackBtn>Ride</BlackBtn>
