03d81f5e1cd38a8d0adc9a8e5fe82279f1cf6a42
Kosisochukwu <realitynuel@gmail.com>
2024-08-04 19:09:11 +0100

    Completed the Our services responsiveness

--- safe-ride/src/components/OurServices/OurServices.jsx
+++ safe-ride/src/components/OurServices/OurServices.jsx
@@ -10,54 +10,52 @@ const OurServices = () => {
     <section className="services-container">
       <h1 className="services-title">our services</h1>
       <p className="services-text">
-      Try any of our services to know the reliability and genuineness
+        Try any of our services to know the reliability and genuineness
       </p>
       <section className="services-container-wrapper">
         <section className="service-wrapper one">
-          <article className="service-container-one">
-            <article className="service-container-one-title">
+          <article className="service-container-one one">
+            <article className="service-container-one-title one">
               <h2>Rides</h2>
               <p>Request and get a quick <br />and safe ride</p>
             </article>
-            <article className="service-container-one-img">
+            <article className="service-container-one-img one">
               <img src={image1} alt="bg-one" />
             </article>
           </article>
-          <article className="service-container-one">
-            <article className="service-container-one-title">
+          <article className="service-container-one two">
+            <article className="service-container-one-title two">
               <h2>Delivery</h2>
               <p>SafeRide makes car renting
-              easy</p>
-            
+                easy</p>
             </article>
-            <article className="service-container-one-img img-two">
+            <article className="service-container-one-img two img-two">
               <img src={image2} alt="bg-one" />
             </article>
           </article>
         </section>
         <section className="service-wrapper two">
-          <article className="service-container-one">
-            <article className="service-container-one-title">
+          <article className="service-container-one three">
+            <article className="service-container-one-title three">
               <h2>Car-sharing</h2>
               <p>Choose your favorite food,
-              delivered fast</p>
+                delivered fast</p>
             </article>
-            <article className="service-container-one-img img-two img-three">
+            <article className="service-container-one-img three img-three">
               <img src={image3} alt="bg-one" />
             </article>
           </article>
-          <article className="service-container-one">
-            <article className="service-container-one-title">
+          <article className="service-container-one four">
+            <article className="service-container-one-title four">
               <h2>Business</h2>
               <p>Partner with reliable and
-              safe company</p>
+                safe company</p>
             </article>
-            <article className="service-container-one-img img-two">
+            <article className="service-container-one-img four img-two">
               <img src={image4} alt="bg-one" />
             </article>
           </article>
         </section>
-
       </section>
     </section>
   );
