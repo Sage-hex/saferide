6acaf7416ecbe555eac91005a25fd2518eb8b332
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2024-12-21 12:17:27 +0100

    Remove node_modules from the repository

--- safe-ride/src/components/Layout/Layout.jsx
+++ safe-ride/src/components/Layout/Layout.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Header from '../Header/Header'
-import 
+
 
 const Layout = () => {
   return (
     <section>
-      
+      <h2>Hi am I a victor </h2>
     </section>
   )
 }
 
 export default Layout
+
+// const BUY_CAKE = 'BUY_CAKE';
+
+// {
+//   type: BUY_CAKE,
+//   payload: {
+//     flavor: 'chocolate',
+//     quantity: 10,
+//   },
+// }
